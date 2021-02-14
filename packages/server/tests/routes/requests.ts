828003bd6d2f359f1d42497200c94e387808bf38
toxeeec <bartosz.kapciak@gmail.com>
2021-02-14 16:48:50 +0100

    Fix friend requests tests and add test case

--- packages/server/tests/routes/requests.ts
+++ packages/server/tests/routes/requests.ts
@@ -3,23 +3,25 @@ import faker from 'faker';
 import supertest from 'supertest';
 import { getConnection } from 'typeorm';
 import { User } from '../../dist/entity/User';
+import createTokens from '../../dist/helpers/createTokens';
 import createTypeOrmConnection from '../../dist/helpers/createTypeOrmConnection';
 import app from '../../dist/server';
 
 const request = supertest(app);
 
 interface IUser {
+  id?: string;
   name: string;
   surname: string;
   email: string;
   password?: string;
+  accessToken?: string;
 }
 
-let dbUsers: User[];
+const users: IUser[] = [];
 
 beforeAll(async () => {
   await createTypeOrmConnection();
-  const users: IUser[] = [];
   for (let i = 0; i < 3; i += 1) {
     users.push({
       name: faker.name.firstName(),
@@ -30,21 +32,49 @@ beforeAll(async () => {
   for await (const user of users) {
     user.password = await argon2.hash(faker.internet.password());
   }
-  dbUsers = User.create(users);
+  const dbUsers = User.create(users);
+  for await (const [i, user] of users.entries()) {
+    user.id = dbUsers[i].id;
+    const { accessToken } = await createTokens(user as User);
+    user.accessToken = accessToken;
+  }
   await User.save(dbUsers);
 });
 
 describe('requests route', () => {
   const path = '/api/requests';
   it('should send friend request to the user', async (done) => {
-    const res = await request.post(`${path}/${dbUsers[1].id}`).expect(201);
+    const res = await request
+      .post(`${path}/${users[1].id}`)
+      .auth(users[0].accessToken!, { type: 'bearer' });
+    expect(201);
     expect(res.body).toHaveProperty('message', 'Request sent successfully');
     done();
   });
 
   it('should return 400 error when request to this user was already sent', async (done) => {
-    await request.post(`${path}/${dbUsers[1].id}`);
-    const res = await request.post(`${path}/${dbUsers[1].id}`).expect(400);
+    await request
+      .post(`${path}/${users[1].id}`)
+      .auth(users[0].accessToken!, { type: 'bearer' });
+    const res = await request
+      .post(`${path}/${users[1].id}`)
+      .auth(users[0].accessToken!, { type: 'bearer' })
+      .expect(400);
+    expect(res.body).toHaveProperty(
+      'message',
+      'Request to this user was already sent'
+    );
+    done();
+  });
+
+  it('should return 400 error when this user already sent you a friend request', async (done) => {
+    await request
+      .post(`${path}/${users[0].id}`)
+      .auth(users[1].accessToken!, { type: 'bearer' });
+    const res = await request
+      .post(`${path}/${users[1].id}`)
+      .auth(users[0].accessToken!, { type: 'bearer' })
+      .expect(400);
     expect(res.body).toHaveProperty(
       'message',
       'Request to this user was already sent'
