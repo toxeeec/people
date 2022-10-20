3540c6a2a722c8989a90f097450f96363954add7
toxeeec <bartosz.kapciak@gmail.com>
2022-10-20 16:49:29 +0200

    feat(backend): add basic openapi 3 spec with router

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/toxeeec/people/backend/spec"
+)
+
+type handler struct{}
+
+func (h *handler) Get(c echo.Context) error {
+	return c.JSONBlob(200, []byte(`{"hello": "world"}`))
+}
+
 func main() {
-	println("hello world")
+	e := echo.New()
+	e.GET("openapi.json", func(c echo.Context) error {
+		spec, err := spec.GetSwagger()
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+		return c.JSON(http.StatusOK, spec)
+	})
+	spec.RegisterHandlers(e, &handler{})
+	e.Logger.Fatal(e.Start(":8000"))
 }
