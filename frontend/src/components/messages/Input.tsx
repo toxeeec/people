2c5445dfa5e7833763f30416877f48e13e0844e3
toxeeec <bartosz.kapciak@gmail.com>
2023-02-14 18:04:32 +0100

    feat(frontend): add emoji picker

--- frontend/src/components/messages/Input.tsx
+++ frontend/src/components/messages/Input.tsx
@@ -1,7 +1,8 @@
-import { ActionIcon, Flex, Textarea } from "@mantine/core";
+import { ActionIcon, Box, Flex, Textarea } from "@mantine/core";
 import { useEventListener } from "@mantine/hooks";
-import { IconSend } from "@tabler/icons";
-import { Dispatch, SetStateAction, useContext, useMemo } from "react";
+import { IconMoodSmile, IconSend } from "@tabler/icons";
+import Picker from "@emoji-mart/react";
+import { Dispatch, SetStateAction, useContext, useMemo, useState } from "react";
 import { NotificationsContext } from "../../context/NotificationsContext";
 
 interface InputProps {
@@ -11,6 +12,7 @@ interface InputProps {
 }
 export const Input = ({ message, setMessage, to }: InputProps) => {
 	const { sendMessage } = useContext(NotificationsContext);
+	const [opened, setOpened] = useState(false);
 	const empty = useMemo(() => message.trim().length === 0, [message]);
 	const handleSubmit = () => {
 		if (empty) return;
@@ -26,20 +28,52 @@ export const Input = ({ message, setMessage, to }: InputProps) => {
 	const ref = useEventListener("keypress", handleKeypress);
 
 	return (
-		<Flex ref={ref} bottom={0} w="100%" align="flex-end" pt="sm">
-			<Textarea
-				autosize
-				minRows={1}
-				maxRows={5}
-				style={{ flex: 1 }}
-				value={message}
-				onChange={(e) => setMessage(e.currentTarget.value)}
-			/>
-			{!empty && (
-				<ActionIcon m={4} onClick={handleSubmit}>
+		<>
+			<Flex ref={ref} bottom={0} w="100%" align="center" mt="sm">
+				<Textarea
+					autosize
+					minRows={1}
+					maxRows={5}
+					value={message}
+					onChange={(e) => setMessage(e.currentTarget.value)}
+					ml="xs"
+					styles={{
+						input: {
+							border: "none",
+							borderRadius: 21,
+							overflow: "hidden",
+							paddingRight: 28,
+							fontSize: 16,
+						},
+						root: { flex: 1 },
+					}}
+				/>
+				<Box hidden={!opened} pos="absolute" right={39} bottom={42}>
+					<Picker
+						onClickOutside={() => opened && setOpened(false)}
+						onEmojiSelect={(e: { native: string }) => {
+							setMessage((msg) => msg.concat(e.native));
+						}}
+					/>
+				</Box>
+				<ActionIcon
+					pos="absolute"
+					right={39}
+					bottom={8}
+					ml="auto"
+					onClick={() => setOpened((o) => !o)}
+				>
+					<IconMoodSmile />
+				</ActionIcon>
+				<ActionIcon
+					m={4}
+					onClick={handleSubmit}
+					disabled={empty}
+					style={{ background: "none", border: "none" }}
+				>
 					<IconSend />
 				</ActionIcon>
-			)}
-		</Flex>
+			</Flex>
+		</>
 	);
 };
